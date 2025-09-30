5264a342447cbf625bd81b0b6f5a668b7e73ee5e
Warren R. Bank <warren.r.bank@gmail.com>
2025-09-30 15:58:43 -0700

    combine strategies to query the Telephony ContentProvider
    1. query for specific transaction
       * if found, use this result
    2. query by timestamp
       * fallback strategy
       * it is possible to find the wrong result,
         though highly unlikely
       * if found, use this result
    3. if not found:
       * wait 1 second and then try again
       * stop trying after 1 minute

--- android-studio-project/MMS-Forwarding/src/main/java/com/github/warren_bank/mms_automatic_forwarding/event/MMSReceiver.java
+++ android-studio-project/MMS-Forwarding/src/main/java/com/github/warren_bank/mms_automatic_forwarding/event/MMSReceiver.java
@@ -74,20 +74,50 @@ public class MMSReceiver extends BroadcastReceiver {
 
         // begin database lookup from Telephony ContentProvider
 
+        final String tr_id = new String(pdu.getTransactionId());
+        final String ct_l  = new String(pdu.getContentLocation());
+
         final String timestamp = Integer.toString(
           (int)(System.currentTimeMillis() / 1000) - 2
         );
 
         for (int i = 0; i < MAX_TRIES; i++) {
-          Cursor cursor = context.getContentResolver().query(
-            Mms.Inbox.CONTENT_URI,
-            new String[]{Mms._ID},
-            (Mms.DATE + ">=?"),
-            new String[]{timestamp},
-            Mms.DEFAULT_SORT_ORDER
-          );
+          Cursor cursor = null;
 
-          if ((cursor != null) && (cursor.getCount() > 0) && cursor.moveToFirst()) {
+          if ((cursor == null) || (cursor.getCount() == 0) || !cursor.moveToFirst()) {
+            if (!tr_id.isEmpty() && !ct_l.isEmpty()) {
+              cursor = context.getContentResolver().query(
+                Mms.Inbox.CONTENT_URI,
+                new String[]{Mms._ID},
+                (Mms.TRANSACTION_ID + "=? AND " + Mms.CONTENT_LOCATION + "=? AND " + Mms._ID + " IS NOT NULL"),
+                new String[]{tr_id, ct_l},
+                null
+              );
+            }
+          }
+
+          if ((cursor == null) || (cursor.getCount() == 0) || !cursor.moveToFirst()) {
+            if (!timestamp.isEmpty()) {
+              cursor = context.getContentResolver().query(
+                Mms.Inbox.CONTENT_URI,
+                new String[]{Mms._ID},
+                (Mms.DATE + ">=? AND " + Mms._ID + " IS NOT NULL"),
+                new String[]{timestamp},
+                Mms.DEFAULT_SORT_ORDER
+              );
+            }
+          }
+
+          if ((cursor == null) || (cursor.getCount() == 0) || !cursor.moveToFirst()) {
+            try {
+              Thread.sleep(SLEEP_MS);
+              continue;
+            }
+            catch(InterruptedException e) {
+              break;
+            }
+          }
+          else {
             try {
               String msgId = cursor.getString(cursor.getColumnIndex(Mms._ID));
               cursor.close();
@@ -101,6 +131,7 @@ public class MMSReceiver extends BroadcastReceiver {
                 }
               }
               else {
+                // should never happen
                 Thread.sleep(SLEEP_MS);
                 continue;
               }
@@ -108,15 +139,6 @@ public class MMSReceiver extends BroadcastReceiver {
             catch(Exception e) {
             }
             break;
-          }
-          else {
-            try {
-              Thread.sleep(SLEEP_MS);
-              continue;
-            }
-            catch(InterruptedException e) {
-              break;
-            }
           }
         }
       }
